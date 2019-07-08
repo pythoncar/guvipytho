b2ba4299cc6006d22d317e7ea5d1794614d33fdd
Karthikeyan <50619753+pythoncar@users.noreply.github.com>
2019-07-09 01:54:01 +0530

    Update and rename 2  set19 .py to 2  set--19 .py

---
+++ 2  set--19 .py
@@ -0,0 +1,7 @@
+kar=int(input())
+
+temp=1
+while kar>0:
+    temp=temp*kar
+    kar-=1
+print(temp)
--- 2  set19 .py
+++
@@ -1,7 +0,0 @@
-kc=int(input())
-
-tempc=1
-while kc>0:
-    temp=tempc*kc
-    kc-=1
-print(tempc)
